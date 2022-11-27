ba7ada73c93c292c7ddaef5af300c4b3f46eee7a
Abdulaziz <azozb997@gmail.com>
2022-11-27 03:25:14 +0300

    wrapped myapp return in StateContext to share the states and toaster to demonstrate announcement

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import { Toaster } from 'react-hot-toast'
+
+import {StateContext} from '../Context/StateContext'
 
 function MyApp({ Component, pageProps }) {
   return(
+    <StateContext>
     <Layout>
+    <Toaster />
      <Component {...pageProps} />
     </Layout>
+    </StateContext>
+
      )
 }
 
