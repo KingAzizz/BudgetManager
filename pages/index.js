e023880e5ddb610c82897ed4a80f57fbe210d5ac
Abdulaziz <azozb997@gmail.com>
2022-11-27 03:27:46 +0300

    used ref hook  to the wallet div

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useRef, useState } from 'react'
 import Wallet from '../components/Wallet'
 
 export default function Home() {
+  let walletRef = useRef()
+
+  
+
+  
   return (
     <>
-     <div className='wallet-container'>
+     <div className='wallet-container' ref={walletRef}>
     <Wallet />
      </div>
     </>
