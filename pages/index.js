54f595c3b2566a8c0123af6f0a92e22000c51c0a
Abdulaziz <azozb997@gmail.com>
2022-11-29 05:15:39 +0300

    added a new component "SpendingTracker" to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useRef, useState } from 'react'
+import SpendingTracker from '../components/SpendingTracker'
 import Wallet from '../components/Wallet'
 
 export default function Home() {
@@ -13,6 +14,9 @@ export default function Home() {
     <>
      <div className='wallet-container' ref={walletRef}>
     <Wallet />
+     </div>
+     <div className="spendingTrackerContainer">
+      <SpendingTracker />
      </div>
     </>
   )
