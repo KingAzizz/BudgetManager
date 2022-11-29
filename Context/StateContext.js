c7548ca40c04c33e14c3c04869fc5abd6ddc4792
Abdulaziz <azozb997@gmail.com>
2022-11-29 05:14:30 +0300

    added a function for adding purchases or spending and saving what the user enters, such as balance, spending details, and so on, to the local storage

--- Context/StateContext.js
+++ Context/StateContext.js
@@ -1,6 +1,6 @@
 
 import React,{createContext,useContext,useState,useEffect,useRef} from 'react'
-
+import toast from 'react-hot-toast'
 
 const Context = createContext()
 
@@ -10,25 +10,75 @@ export const StateContext = ({children}) => {
     const [balance, setBalance] = useState(0)
     const [saving,setSaving] = useState(0)
     const [spending, setSpending] = useState(0)
-    const [typeOfSpending,setTypeOfSpending] = useState("")
-    const [date,setDate] = useState(null)
-    const [name,setName] = useState("")
+    const [historySpending,setHistorySpending] = useState([])
 
-    const addBalanceSavingsAmount = (type,amount) => {
+    useEffect(() => {
+        // localStorage.clear()
+        localStorage.getItem("balance") && setBalance(parseFloat(localStorage.getItem("balance")))
+        localStorage.getItem("saving") &&  setSaving(parseFloat(localStorage.getItem("saving")))
+        localStorage.getItem("spending") && setSpending(parseFloat(localStorage.getItem("spending")))
+    },[])
+    useEffect(() => {
+        
+        setHistorySpending(JSON.parse(localStorage.getItem("spendDetails")))
+    },[spending])
+    
+    const addBalanceSavingsAmount = (type,amount,hideAdd) => {
         if(type === "Saving"){
             setSaving((prevAmount) => prevAmount + amount)
+            hideAdd(false)
+            localStorage.setItem("saving",saving + amount)
         }
 
         else if(type === "Balance"){
             setBalance((prevAmount) => prevAmount + amount)
+            hideAdd(false)
+            localStorage.setItem("balance",balance + amount)
+
+
         }
     }
+
+    const addSpending = (amountSpend,date,type,setDate,setCurrentSpend,setTypeOfSpending) => {
+        
+        if(amountSpend === "" || date === "" || type === ""){
+            toast.error("Fields are empty")
+
+        }else{
+
+            setBalance((prevAmount) => prevAmount - parseFloat(amountSpend))
+            localStorage.setItem("balance",balance - parseFloat(amountSpend))
+            setSpending((prev) => prev + parseFloat(amountSpend))
+            localStorage.setItem("spending",spending + parseFloat(amountSpend))
+            let spendDetails ={
+                type:type,
+                date:date,
+                amountSpend:amountSpend
+            }
+          
+            if(localStorage.getItem("spendDetails") !== null){
+
+                localStorage.setItem("spendDetails",JSON.stringify([...historySpending,spendDetails]))
+            }else{
+                localStorage.setItem("spendDetails",JSON.stringify([spendDetails]))
+
+            }
+            setTypeOfSpending("")
+            setDate("")
+            setCurrentSpend(0)
+        }
+        }
     
 return (<Context.Provider value={{
     balance,
     saving,
     spending,
-    addBalanceSavingsAmount
+    setSpending,
+    addBalanceSavingsAmount,
+    addSpending
+,historySpending
+   
+    
 }}>
 
 
