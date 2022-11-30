e42ff920d44c590d8c53745361d9992f30741d14
Abdulaziz <azozb997@gmail.com>
2022-11-30 06:37:33 +0300

    meta tags

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,6 +7,25 @@ const Navbar = () => {
         <Head>
             <title>Budget Manager</title>
             <link rel="icon" type="image/x-icon" href="/logoColor.png" />
+
+<meta name="title" content="Budget Manager" />
+<meta name="description" content="Manage your spending monitor your expenses to see where your money is being wasted
+" />
+
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://budget-manager-beige.vercel.app/" />
+<meta property="og:title" content="Budget Manager" />
+<meta property="og:description" content="Manage your spending monitor your expenses to see where your money is being wasted
+" />
+<meta property="og:image" content="/logoColor.png" />
+
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="https://budget-manager-beige.vercel.app/" />
+<meta property="twitter:title" content="Budget Manager" />
+<meta property="twitter:description" content="Manage your spending monitor your expenses to see where your money is being wasted
+" />
+<meta property="twitter:image" content="/logoColor.png"></meta>
+
             </Head>
         <Image src="/logo.png" width={102} height={70} alt="logo image"/>
     </nav>
